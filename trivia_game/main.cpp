a48ad51915413fe62c635ad62c8e92d093864a76
Sn-goat <sergile-junior.domguia-nyouvop@polymtl.ca>
2024-06-19 15:01:47 -0400

    starting

--- trivia_game/main.cpp
+++ trivia_game/main.cpp
@@ -1,23 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
-#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "trivia_game_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
     MainWindow w;
 
     w.show();
